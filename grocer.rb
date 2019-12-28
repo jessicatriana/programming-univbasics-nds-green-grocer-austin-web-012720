20182ad1142a4cf3b6d25991b19fcef573e2d36f
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-28 23:18:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,7 +76,7 @@ def checkout(cart, coupons)
   total = 0 
   index = 0 
   while index < final_cart do 
-    total +=   
+    total += final_cart[index][:price] * final_cart[index][count]
     index += 1
   end
 end
